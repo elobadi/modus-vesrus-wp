6fa0a7ee31c33a2a93c1b2dd767952a6c2e1c121
Mohammed Elobadi <elobadi@gmail.com>
2018-11-19 15:33:28 +0200

    made the Showcase "carousel" dynamic.

--- wp-modus/page-home.php
+++ wp-modus/page-home.php
@@ -14,27 +14,43 @@ get_header();
 				<li data-target="#myCarousel" data-slide-to="2"></li>
 			</ol>
 			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="<?php bloginfo('stylesheet_directory'); ?>\img\img-1.jpeg" alt="First slide">
-					<div class="carousel-caption d-none d-md-block">
-						<h1 class="display-4">VESTIBULUM</h1>
-						<p>Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
+
+				 <!-- First image "Slide 1". -->
+				<?php $slide_1 = get_field( 'slide_1' ); ?>
+				<?php if ( $slide_1 ) { ?>
+					<div class="carousel-item active">
+						<img class="d-block w-100" src="<?php echo $slide_1['url']; ?>" alt="<?php echo $slide_1['alt']; ?>">
+						<div class="carousel-caption d-none d-md-block">
+							<h1 class="display-4"><?php the_field( 'slider_1_heading' ); ?></h1>
+							<p><?php the_field( 'slider_1_description' ); ?></p>
+						</div>
 					</div>
-				</div>
-				<div class="carousel-item">
-					<img class="d-block w-100" src="<?php bloginfo('stylesheet_directory'); ?>\img\img-2.jpeg" alt="Second slide">
-					<div class="carousel-caption d-none d-md-block">
-						<h1 class="display-4">VESTIBULUM</h1>
-						<p class="lead">Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
+				<?php } ?>
+
+				 <!-- Second image "Slide 2". -->
+				<?php $slide_2 = get_field( 'slide_2' ); ?>
+				<?php if ( $slide_2 ) { ?>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="<?php echo $slide_2['url']; ?>" alt="<?php echo $slide_2['alt']; ?>">
+						<div class="carousel-caption d-none d-md-block">
+							<h1 class="display-4"><?php the_field( 'slider_2_heading' ); ?></h1>
+							<p class="lead"><?php the_field( 'slider_2_description' ); ?></p>
+						</div>
 					</div>
-				</div>
-				<div class="carousel-item">
-					<img class="d-block w-100" src="<?php bloginfo('stylesheet_directory'); ?>\img\img-3.jpeg" alt="Third slide">
-					<div class="carousel-caption d-none d-md-block">
-						<h1 class="display-4">VESTIBULUM</h1>
-						<p>Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</p>
+				<?php } ?>
+
+				 <!-- Third image "Slide 3". -->
+				 <?php $slide_3 = get_field( 'slide_3' ); ?>
+				<?php if ( $slide_3 ) { ?>
+					<div class="carousel-item">
+						<img class="d-block w-100" src="<?php echo $slide_3['url']; ?>" alt="<?php echo $slide_3['alt']; ?>">
+						<div class="carousel-caption d-none d-md-block">
+							<h1 class="display-4"><?php the_field( 'slider_3_heading' ); ?></h1>
+							<p><?php the_field( 'slider_3_description' ); ?></p>
+						</div>
 					</div>
-				</div>
+				<?php } ?>
+
 			</div>
 			<!-- .carousel-inner -->
 

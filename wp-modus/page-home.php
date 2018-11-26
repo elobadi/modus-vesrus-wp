9930480ac4e49c430b0fdd1a7e3c107a7a976951
Mohammed Elobadi <elobadi@gmail.com>
2018-11-26 22:25:52 +0200

    Made the 'intro' section dynamic.

--- wp-modus/page-home.php
+++ wp-modus/page-home.php
@@ -79,20 +79,23 @@ get_header();
 	=========================================== -->
 
     <!-- INTRO -->
+<?php $intro_button = get_field( 'intro_button' ); ?>
+<?php if ( $intro_button ) { ?>
     <section id="intro">
         <div class="container mt-5">
             <div class="row">
                 <div class="col-xl-10 col-lg-9 text-center text-lg-left p-lg-0">
-                    <h3>Some of our top services</h3>
-                    <p>Ut eleifend libero sed neque rhoncus consequat. Maecenas tincidunt, augue et rutrum condimentum,
-                        libero lectus mattis orci, ut commodo. </p>
+                    <h3><?php the_field( 'intro_title' ); ?></h3>
+                    <p><?php the_field( 'intro_description' ); ?></p>
                 </div>
                 <div class="col-xl-2 col-lg-3 text-center text-lg-right p-lg-0">
-                    <a role="button" href="services.html" class="btn btn-lg mt-3">VIEW MORE</a>
+                    <a role="button" href="<?php echo $intro_button['url']; ?>"
+                       class="btn btn-lg mt-3"><?php echo $intro_button['title']; ?></a>
                 </div>
             </div>
         </div>
     </section>
+<?php } ?>
     <!-- #intro -->
     <!-- END OF INTRO
 	=========================================== -->

0d8f8f9c60c56a4b1ac8b7275a4d771ed9fa27aa
Mohammed Elobadi <elobadi@gmail.com>
2019-02-21 15:06:48 +0200

    Fixed the 'items' Section in 'page-portfolio-2col.php'.

--- wp-modus/page-portfolio-2col.php
+++ wp-modus/page-portfolio-2col.php
@@ -19,8 +19,11 @@ get_header();
 				'order'     => 'ASC'
 			) ); ?>
 			<?php while ( $loop->have_posts() ) :
-				$loop->the_post(); ?>
-                <div class="card-deck mb-md-5">
+			$loop->the_post(); ?>
+			<?php if ( ( $loop->current_post + 1 ) % 2 === 1 ) { ?>
+            <div class="card-deck mb-md-5">
+				<?php } ?>
+				<?php if ( ( $loop->current_post + 1 ) != ( $loop->post_count ) ) { ?>
                     <div class="card border-0 ml-0">
                         <div class="card-body p-2">
 							<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
@@ -52,45 +55,51 @@ get_header();
 							<?php } ?>
                         </div>
                     </div>
+				<?php } else { ?>
+                <div class="card border-0 ml-0">
+                    <div class="card-body p-2">
+						<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
+						<?php if ( $portfolio_image ) { ?>
+                            <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
+                                 alt="<?php echo $portfolio_image['alt']; ?>">
+                            <div class="overlay">
+                                <div class="container">
+                                    <ul class="overlay-links list-inline">
 
-                    <div class="card border-0 ml-0">
-                        <div class="card-body p-2">
-							<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
-							<?php if ( $portfolio_image ) { ?>
-                                <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
-                                     alt="<?php echo $portfolio_image['alt']; ?>">
-                                <div class="overlay">
-                                    <div class="container">
-                                        <ul class="overlay-links list-inline">
-
-											<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
-											<?php if ( $portfolio_link ) { ?>
-                                                <a href="<?php echo $portfolio_link['url']; ?>">
-                                                    <li class="list-inline-item"><i
-                                                                class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
-                                                    </li>
-                                                </a>
-                                                <a href="#">
-                                                    <li class="list-inline-item"><i
-                                                                class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
-                                                    </li>
-                                                </a>
-											<?php } ?>
-                                        </ul>
-                                        <p class="overlay-info">
-                                        <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
-                                    </div>
+										<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
+										<?php if ( $portfolio_link ) { ?>
+                                            <a href="<?php echo $portfolio_link['url']; ?>">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+                                            <a href="#">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+										<?php } ?>
+                                    </ul>
+                                    <p class="overlay-info">
+                                    <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
                                 </div>
-							<?php } ?>
-                        </div>
+                            </div>
+						<?php } ?>
                     </div>
                 </div>
-			<?php endwhile; ?>
-			<?php wp_reset_query() ?>
+                <div class="card border-0 ml-0"></div>
+            </div>
+		<?php } ?>
+			<?php if ( ( $loop->current_post + 1 ) % 2 === 0 ) { ?>
         </div>
+		<?php } ?>
+		<?php endwhile; ?>
+		<?php wp_reset_query() ?>
+    </div>
     </div>
     <!-- END OF ITEMS
   =========================================== -->
+
 
     <!-- PAGINATION -->
     <div id="pagination">

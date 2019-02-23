33faffc9228d30a7bf88dd34c9e189c39f738a16
Mohammed Elobadi <elobadi@gmail.com>
2019-02-23 20:50:29 +0200

    Made page-portfolio-4col.php dynamic.

--- wp-modus/page-portfolio-4col.php
+++ wp-modus/page-portfolio-4col.php
@@ -10,173 +10,240 @@ get_header();
 
 <?php get_template_part( 'template-parts/content', 'portfolio-top-nav' ); ?>
 
-	<!-- ITEMS -->
-	<div id="items">
-		<div class="container">
+    <!-- ITEMS -->
+    <div id="items" class="four-col">
+        <div class="container">
+			<?php
+			$counter = 1; // start counter
+			$grids   = 4; // Grids per row
+			?>
 			<?php $loop = new WP_Query( array(
 				'post_type' => 'portfolio',
 				'orderby'   => 'post_id',
 				'order'     => 'ASC'
 			) ); ?>
-			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-			<div class="card-deck mb-md-5">
-				<div class="card border-0 ml-0">
-					<div class="card-body p-2">
-						<img src="src/img/placeholder-image1.jpg" class="img-fluid" alt="Responsive image">
-						<div class="overlay">
-							<div class="container">
-								<ul class="overlay-links list-inline">
-									<a href="portfolio-project.html">
-										<li class="list-inline-item"><i class="fa fa-eye"></i></li>
-									</a>
-									<a href="#">
-										<li class="list-inline-item"><i class="fa fa-link"></i></li>
-									</a>
-								</ul>
-								<p class="overlay-info">
-									Lorem ipsum</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="card border-0">
-					<div class="card-body p-2">
-						<img src="src/img/placeholder-image1.jpg" class="img-fluid" alt="Responsive image">
-						<div class="overlay">
-							<div class="container">
-								<ul class="overlay-links list-inline">
-									<a href="portfolio-project.html">
-										<li class="list-inline-item"><i class="fa fa-eye"></i></li>
-									</a>
-									<a href="#">
-										<li class="list-inline-item"><i class="fa fa-link"></i></li>
-									</a>
-								</ul>
-								<p class="overlay-info">
-									Lorem ipsum</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="card border-0">
-					<div class="card-body p-2">
-						<img src="src/img/placeholder-image1.jpg" class="img-fluid" alt="Responsive image">
-						<div class="overlay">
-							<div class="container">
-								<ul class="overlay-links list-inline">
-									<a href="portfolio-project.html">
-										<li class="list-inline-item"><i class="fa fa-eye"></i></li>
-									</a>
-									<a href="#">
-										<li class="list-inline-item"><i class="fa fa-link"></i></li>
-									</a>
-								</ul>
-								<p class="overlay-info">
-									Lorem ipsum</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="card border-0 mr-0">
-					<div class="card-body p-2">
-						<img src="src/img/placeholder-image1.jpg" class="img-fluid" alt="Responsive image">
-						<div class="overlay">
-							<div class="container">
-								<ul class="overlay-links list-inline">
-									<a href="portfolio-project.html">
-										<li class="list-inline-item"><i class="fa fa-eye"></i></li>
-									</a>
-									<a href="#">
-										<li class="list-inline-item"><i class="fa fa-link"></i></li>
-									</a>
-								</ul>
-								<p class="overlay-info">
-									Lorem ipsum</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-            <?php endwhile; ?>
+			<?php while ( $loop->have_posts() ) :
+				$loop->the_post(); ?>
+				<?php if ( $counter === 1 ) { ?>
+                <div class="card-deck mb-md-5">
+			<?php } ?>
+
+				<?php if ( $counter == 1 ) { ?>
+                <div class="card border-0 ml-0">
+                    <div class="card-body p-2">
+						<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
+						<?php if ( $portfolio_image ) { ?>
+                            <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
+                                 alt="<?php echo $portfolio_image['alt']; ?>">
+                            <div class="overlay">
+                                <div class="container">
+                                    <ul class="overlay-links list-inline">
+
+										<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
+										<?php if ( $portfolio_link ) { ?>
+                                            <a href="<?php echo $portfolio_link['url']; ?>">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+                                            <a href="#">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+										<?php } ?>
+                                    </ul>
+                                    <p class="overlay-info">
+                                    <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
+                                </div>
+                            </div>
+						<?php } ?>
+                    </div>
+                </div>
+			<?php } elseif ( $counter == 2 ) { ?>
+                <div class="card border-0">
+                    <div class="card-body p-2">
+						<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
+						<?php if ( $portfolio_image ) { ?>
+                            <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
+                                 alt="<?php echo $portfolio_image['alt']; ?>">
+                            <div class="overlay">
+                                <div class="container">
+                                    <ul class="overlay-links list-inline">
+
+										<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
+										<?php if ( $portfolio_link ) { ?>
+                                            <a href="<?php echo $portfolio_link['url']; ?>">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+                                            <a href="#">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+										<?php } ?>
+                                    </ul>
+                                    <p class="overlay-info">
+                                    <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
+                                </div>
+                            </div>
+						<?php } ?>
+                    </div>
+                </div>
+			<?php } elseif ( $counter == 3 ) { ?>
+                <div class="card border-0">
+                    <div class="card-body p-2">
+						<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
+						<?php if ( $portfolio_image ) { ?>
+                            <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
+                                 alt="<?php echo $portfolio_image['alt']; ?>">
+                            <div class="overlay">
+                                <div class="container">
+                                    <ul class="overlay-links list-inline">
+
+										<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
+										<?php if ( $portfolio_link ) { ?>
+                                            <a href="<?php echo $portfolio_link['url']; ?>">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+                                            <a href="#">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+										<?php } ?>
+                                    </ul>
+                                    <p class="overlay-info">
+                                    <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
+                                </div>
+                            </div>
+						<?php } ?>
+                    </div>
+                </div>
+			<?php } elseif ( $counter == 4 ) { ?>
+                <div class="card border-0 mr-0">
+                    <div class="card-body p-2">
+						<?php $portfolio_image = get_field( 'portfolio_image' ); ?>
+						<?php if ( $portfolio_image ) { ?>
+                            <img src="<?php echo $portfolio_image['url']; ?>" class="img-fluid"
+                                 alt="<?php echo $portfolio_image['alt']; ?>">
+                            <div class="overlay">
+                                <div class="container">
+                                    <ul class="overlay-links list-inline">
+
+										<?php $portfolio_link = get_field( 'portfolio_link' ); ?>
+										<?php if ( $portfolio_link ) { ?>
+                                            <a href="<?php echo $portfolio_link['url']; ?>">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_see_more_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+                                            <a href="#">
+                                                <li class="list-inline-item"><i
+                                                            class="<?php the_field( 'portfolio_link_icon' ); ?>"></i>
+                                                </li>
+                                            </a>
+										<?php } ?>
+                                    </ul>
+                                    <p class="overlay-info">
+                                    <h6><?php the_field( 'portfolio_title' ); ?></h6></p>
+                                </div>
+                            </div>
+						<?php } ?>
+                    </div>
+                </div>
+			<?php } ?>
+
+
+				<?php if ( ( $counter === 4 ) || ( ( $loop->current_post + 1 ) == ( $loop->post_count ) ) ) { ?>
+                </div>
+			<?php } ?>
+				<?php $counter = ( $counter == 4 ) ? 1 : ( $counter + 1 ); ?>
+			<?php endwhile; ?>
 			<?php wp_reset_query() ?>
-		</div>
-	</div>
-	<!-- END OF ITEMS
+        </div>
+    </div>
+    <!-- END OF ITEMS
   =========================================== -->
 
-	<!-- PAGINATION -->
-	<div id="pagination">
-		<div class="container">
-			<div class="row d-none d-md-flex">
-				<div class="col">
-					<nav aria-label="Page navigation example">
-						<ul class="pagination justify-content-center">
-							<li class="page-item">
-								<a class="page-link" href="#"><i class="fa fa-angle-left"></i></a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">1</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link active" href="#">2</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">3</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">4</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">5</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">...</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">10</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">11</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">12</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#"><i class="fa fa-angle-right"></i></a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-			<div class="row d-flex d-md-none">
-				<div class="col">
-					<nav aria-label="Page navigation example">
-						<ul class="pagination justify-content-center">
-							<li class="page-item">
-								<a class="page-link" href="#"><i class="fa fa-angle-left"></i></a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">1</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link active" href="#">2</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">...</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#">12</a>
-							</li>
-							<li class="page-item">
-								<a class="page-link" href="#"><i class="fa fa-angle-right"></i></a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- END OF PAGINATION -->
+
+    <!-- PAGINATION -->
+    <div id="pagination">
+        <div class="container">
+            <div class="row d-none d-md-flex">
+                <div class="col">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#"><i class="fa fa-angle-left"></i></a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link active" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">4</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">5</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">...</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">10</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">11</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">12</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#"><i class="fa fa-angle-right"></i></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="row d-flex d-md-none">
+                <div class="col">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#"><i class="fa fa-angle-left"></i></a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link active" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">...</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">12</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#"><i class="fa fa-angle-right"></i></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END OF PAGINATION -->
 
 <?php
 get_footer();

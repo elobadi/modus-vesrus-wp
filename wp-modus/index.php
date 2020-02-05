942159fff54f545cd2e6706a0edc051e8d298832
Mohammed Elobadi <elobadi@gmail.com>
2020-02-05 19:31:49 +0200

    Deleted unwanted themes & Created '.gitignore'.

--- wp-modus/index.php
+++ wp-modus/index.php
@@ -15,45 +15,58 @@
 get_header();
 ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
+<?php get_template_part('template-parts/content', 'sub-header'); ?>
 
-            <?php
-            if ( have_posts() ) :
+    <div class="blog-listing">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="blog-main">
 
-                if ( is_home() && ! is_front_page() ) :
-                    ?>
-                    <header>
-                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                    </header>
-                <?php
-                endif;
+                        <?php
+                        if (have_posts()) :
 
-                /* Start the Loop */
-                while ( have_posts() ) :
-                    the_post();
+                            if (is_home() && !is_front_page()) :
+                                ?>
+                                <header>
+                                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                                </header>
+                            <?php
+                            endif;
 
-                    /*
-                     * Include the Post-Type-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-                     */
-                    get_template_part( 'template-parts/content', get_post_type() );
+                            /* Start the Loop */
+                            while (have_posts()) :
+                                the_post();
 
-                endwhile;
+                                /*
+                                 * Include the Post-Type-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content', get_post_type());
 
-                the_posts_navigation();
+                            endwhile;
 
-            else :
+                            the_posts_navigation();
 
-                get_template_part( 'template-parts/content', 'none' );
+                        else :
 
-            endif;
-            ?>
+                            get_template_part('template-parts/content', 'none');
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+                        endif;
+                        ?>
+
+                    </div><!-- .blog-main -->
+                </div><!-- .col-lg-9 -->
+                <div class="col-lg-3 d-none d-lg-block">
+                    <div class="blog-sidebar">
+                        <?php get_sidebar(); ?>
+                    </div><!-- .col-lg-3 -->
+                </div><!-- .blog-sidebar -->
+
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .blog-listing -->
 
 <?php
-get_sidebar();
 get_footer();
